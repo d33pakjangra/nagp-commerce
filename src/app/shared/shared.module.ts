48504016da382420eb733b6aa2ea337b8048370e
Deepak Jangra <d33pakjangra@gmail.com>
2021-04-21 12:18:56 +0530

    header, footer, page-layout barebone components added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    PageLayoutComponent
+  ],
   imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
   entryComponents: [],
   exports: [MaterialModule],
