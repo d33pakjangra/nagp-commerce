b981eb00ba3ed33f8db6158f131af6826ae0646b
Deepak Jangra <d33pakjangra@gmail.com>
2021-04-21 15:43:25 +0530

    internationalization added

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 import { Subject } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -7,16 +9,32 @@ import { Subject } from 'rxjs';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild(MatMenuTrigger) languagesMenuTrigger: MatMenuTrigger;
+  @ViewChild('searchText', { static: false }) searchControl: HTMLInputElement;
+
   headerName: string = 'NAGP Commerce';
   searchText: string;
   public onSearchTextChanged = new Subject<string>();
 
-  constructor() {}
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'hin']);
+    translate.setDefaultLang('en');
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|hin/) ? browserLang : 'en');
+  }
 
   ngOnInit(): void {}
 
   clearSearch() {
     this.searchText = null;
     this.onSearchTextChanged.next(null);
+  }
+
+  openLanguagesMenu() {
+    this.languagesMenuTrigger.openMenu();
+  }
+
+  changeLanguage(language: string) {
+    this.translate.use(language);
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,30 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, PageLayoutComponent],
-  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule, RouterModule],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   entryComponents: [],
   exports: [MaterialModule],
 })
