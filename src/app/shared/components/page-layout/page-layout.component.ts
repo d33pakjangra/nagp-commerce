27b79a74c2b60dd18bcb32d5a920984d0713857c
Deepak Jangra <d33pakjangra@gmail.com>
2021-04-30 17:51:18 +0530

    loader on route change

--- src/app/shared/components/page-layout/page-layout.component.ts
+++ src/app/shared/components/page-layout/page-layout.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
+import { LoaderService } from 'src/app/core/services/loader.service';
 
 @Component({
   selector: 'app-page-layout',
   templateUrl: './page-layout.component.html',
-  styleUrls: ['./page-layout.component.scss']
+  styleUrls: ['./page-layout.component.scss'],
 })
 export class PageLayoutComponent implements OnInit {
+  constructor(private readonly router: Router, private readonly loader: LoaderService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof RouteConfigLoadStart) {
+        this.loader.showLoader();
+      } else if (event instanceof RouteConfigLoadEnd) {
+        this.loader.hideLoader();
+      }
+    });
   }
-
 }
