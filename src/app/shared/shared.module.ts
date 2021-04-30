689b1fb861ea953ac704608d3fdcf86934f150e8
Deepak Jangra <d33pakjangra@gmail.com>
2021-04-30 11:05:04 +0530

    footer removed from application

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
@@ -11,14 +10,7 @@ import { ElevationDirective } from './directives/elevation.directive';
 import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
 import { NumberOnlyDirective } from './directives/number-only.directive';
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    PageLayoutComponent,
-    ElevationDirective,
-    NumberOnlyDirective,
-    ConfirmationModalComponent,
-  ],
+  declarations: [HeaderComponent, PageLayoutComponent, ElevationDirective, NumberOnlyDirective, ConfirmationModalComponent],
   imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule, RouterModule, TranslateModule],
   entryComponents: [],
   exports: [
